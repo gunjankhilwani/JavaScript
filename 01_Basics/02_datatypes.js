9a9c0d0ceb6838e8a1fb1cdfa982a7120b3aad36
Gunjan Khilwani <72569317+gunjankhilwani@users.noreply.github.com>
2024-03-14 15:26:12 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  // treat all JS code as newer version
+
+let name = "Gunjan"
+let age = 18
+let isLoggedIn = false
+
+//datatypes:
+// number : 2 to power 53
+// string : " " or ''
+// boolen : true false
+// bigint
+// null : standalone value, empty value, Type Of OBJECT
+// undefined : value is not defined, on;y declared
+// symbol : to find uniqueness
+
+console.log(typeof "gunjan");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
