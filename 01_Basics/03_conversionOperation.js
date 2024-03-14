88585f695f4f4a97f273b14e018c0cb7fac3b09d
Gunjan Khilwani <72569317+gunjankhilwani@users.noreply.github.com>
2024-03-14 17:35:19 +0000

    Conversion Of Datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+//let score = "33"
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+
+
+let score = "gunjan"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+//convert it to number
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// notes
+// "33" => 33
+// "33abc" => NaN
+// true =>1 false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = "gunjan"
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+//notes 
+// 1 => true 0=> fdalse
+// "" => false
+//"gunjan" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
