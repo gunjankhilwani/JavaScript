7ca1d13c232e0b77bb58cd852771864ed5fd781c
Gunjan Khilwani <72569317+gunjankhilwani@users.noreply.github.com>
2024-03-14 20:19:10 +0000

    Datatypes Summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,43 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+//Symbol : unique
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);  //seems equal but its not 
+
+const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+console.log(typeof bigNumber);
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
+console.log(typeof id);
+console.log(typeof outsideTemp);
+console.log(typeof isLoggedIn);
