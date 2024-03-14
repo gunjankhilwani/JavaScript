3ee5a9074f6b3394318db1699e8ae0df1b04003f
Gunjan Khilwani <72569317+gunjankhilwani@users.noreply.github.com>
2024-03-14 18:29:58 +0000

    Operations

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "gunjan"
+let str2 = " khilwani"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
