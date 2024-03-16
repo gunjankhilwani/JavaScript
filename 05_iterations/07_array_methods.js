010b6f177ba18b1d4d7f3fcd56b8052202333846
Gunjan Khilwani <72569317+gunjankhilwani@users.noreply.github.com>
2024-03-16 19:09:45 +0000

    Array Method : map

---
+++ 05_iterations/07_array_methods.js
@@ -0,0 +1,22 @@
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNum.map( (num) => num+10 )
+//or
+// const newNums = myNum.map( (num) => { return num+10 } )
+// console.log(newNums);
+
+//using for each
+// const newNums = []
+// myNum.forEach((nums) => {
+//     nums+=10
+//     newNums.push(nums)
+
+// })
+// console.log(newNums)
+
+//chaining 
+const newNums = myNum
+                .map((num) => num * 10 )
+                .map((num) => num + 1)
+                 .filter((num) => num > 40)
+
+console.log(newNums)
