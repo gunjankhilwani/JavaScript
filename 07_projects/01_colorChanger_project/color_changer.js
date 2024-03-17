a66522b3d0f2914f5571d3394f61e8084cf3b511
Gunjan Khilwani <72569317+gunjankhilwani@users.noreply.github.com>
2024-03-17 18:41:16 +0000

    BgColor Switcher Js code

---
+++ 07_projects/01_colorChanger_project/color_changer.js
@@ -0,0 +1,26 @@
+const btn = document.querySelectorAll(".button")
+//console.log(btn);
+const body = document.querySelector("body")
+
+btn.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id       //or "grey"   
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id       //or "grey"   
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id       //or "grey"   
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id       //or "grey"   
+        }
+    })
+
+});
+
+
